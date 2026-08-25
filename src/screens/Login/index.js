250f5e9fcbfe5016ff88a220a59ec42991110eb7
GoBrazill <vinicius.daraujos26@gmail.com>
2026-08-25 11:10:11 -0300

    feat: finish login screen

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,16 +1,35 @@
-import { Image, Text, TextInput, View } from "react-native"
+import { Image, Pressable, Text, TextInput, View } from "react-native"
 import logotype from '../../assets/icons/logotype.png'
 import { styles } from "./style"
 
 export const Login = () => {
-    return(
-        <View style={styles.view}>
+    return (
+        <View style={styles.containerHome}>
             <Image source={logotype} />
 
-            <Text style={styles.titulo} >faça login e se <Text style={styles.highlight} >surpreenda</Text></Text>
+            <View style={styles.contentHome}>
+                <Text style={styles.title}>faça login e se <Text style={styles.highlight} >surpreenda</Text></Text>
 
-            <TextInput placeholder="com um placeholder"/>
+                <View style={styles.btnWrapper}>
+                    <View style={styles.btnWrapper}>
+                        <View>
+                            <Text style={styles.label}>E-mail: </Text>
+                            <TextInput keyboardType="email-address" secureTextEntry={true} style={styles.input} />
+                        </View>
+
+                        <View>
+                            <Text style={styles.label}>Senha: </Text>
+                            <TextInput secureTextEntry={true} style={styles.input} />
+                        </View>
+                    </View>
+
+                    <Pressable style={styles.btn}><Text style={styles.txtBtn}>Entrar</Text></Pressable>
+                </View>
+
+                <Text style={styles.createAccountText}>Não tem uma conta? <Text style={styles.highlightLink}>Criar conta</Text></Text>
+            </View>
+
         </View>
-        
+
     )
 }
--- src/screens/Login/style.js
+++ src/screens/Login/style.js
@@ -1,16 +1,52 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    view: {
-        paddingTop: 50,
-        paddingLeft: 20
-    },  
-    titulo: {
+    containerHome: {
+        paddingHorizontal: 20,
+        gap: 50
+    },
+    contentHome: {
+        gap: 40
+    },
+    btnWrapper: {
+        gap: 25
+    },
+    title: {
         color: '#2D3133',
         fontSize: 40,
         fontWeight: 'bold',
     },
     highlight: {
-      color: '#375EE7'  
+        color: '#375EE7'
+    },
+    highlightLink: {
+        color: '#375EE7',
+        textDecorationLine: 'underline'
+    },
+    label: {
+        fontSize: 16
+    },
+    input: {
+        borderWidth: 1,
+        borderRadius: 10
+    },
+    btn: {
+        justifyContent: 'center',
+        alignItems: 'center',
+
+        width: '100%',
+        height: 66,
+
+        borderRadius: 10,
+        backgroundColor: '#375EE7'
+    },
+    txtBtn: {
+        fontSize: 20,
+        fontWeight: 'bold',
+
+        color: '#fff'
+    },
+    createAccountText: {
+        textAlign: 'center',
     }
 })
