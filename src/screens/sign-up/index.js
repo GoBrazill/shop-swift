d87ac11c2051ef18156588a77bf95efa0390d0ef
GoBrazill <vinicius.daraujos26@gmail.com>
2026-09-01 11:08:32 -0300

    feat: finish sign up screen

--- src/screens/sign-up/index.js
+++ src/screens/sign-up/index.js
@@ -1,37 +1,52 @@
 import { View, Image, Text, TextInput, Pressable } from "react-native"
 import logoType from '../../assets/icons/logotype.png'
-import styles, { Title } from "./style"
-import { Container } from "./style"
+import { Container, Button, ButtonText, FormContainer, InputsContainer, TextHighlight, Title, Input, TextLabel, Container2 } from "./style"
 
 export const SignUp = () => {
 
     return (
-        // <View style={styles.container}>
-        //     <Image source={logoType} />
-
-        //     <View>
-        //         <Text style={styles.title}>Crie uma conta <Text style={styles.highlight}>e se surpreenda</Text></Text>
-
-        //         <View style={styles.formWrapper}>
-        //             <View>
-        //                 <Text style={styles.label}>E-mail:</Text>
-        //                 <TextInput keyboardType="email-address" style={styles.input}></TextInput>
-        //             </View>
-        //             <View>
-        //                 <Text style={styles.label}>Senha:</Text>
-        //                 <TextInput keyboardType="decimal-pad" style={styles.input}></TextInput>
-        //             </View>
-
-        //             <View style={styles.btnWrapper}>
-        //                 <Pressable style={styles.btn}><Text style={styles.textBtn}>Cadastrar</Text></Pressable>
-        //                 <Text style={styles.loginText}>Já tem uma conta? <Text style={styles.highlightLink}>Fazer login</Text></Text>
-        //             </View>
-        //         </View>
-
-        //     </View>
-        // </View>
         <Container>
-            <Title>Crie uma conta e se surpreenda</Title>
-        </Container>       
+            <Image source={logoType} />
+            <Container2>
+            <Title>Crie uma conta <TextHighlight>e se surpreenda</TextHighlight></Title>
+
+            <FormContainer>
+                <InputsContainer>
+                    <TextLabel>E-mail:</TextLabel>
+                    <Input keyboardType="email-address" />
+
+                    <TextLabel>Senha:</TextLabel>
+                    <Input keyboardType="default" secureTextEntry={true} />
+                </InputsContainer>
+            </FormContainer>
+
+                <Button><ButtonText>Cadastrar</ButtonText></Button>
+            </Container2>
+        </Container>
     )
 }
+// import styles, { Button, ButtonText, FormContainer, Input, InputsContainer, TextHighlight, TextLabel, Title } from "./style"
+// <View style={styles.container}>
+//     <Image source={logoType} />
+
+//     <View>
+//         <Text style={styles.title}>Crie uma conta <Text style={styles.highlight}>e se surpreenda</Text></Text>
+
+//         <View style={styles.formWrapper}>
+//             <View>
+//                 <Text style={styles.label}>E-mail:</Text>
+//                 <TextInput keyboardType="email-address" style={styles.input}></TextInput>
+//             </View>
+//             <View>
+//                 <Text style={styles.label}>Senha:</Text>
+//                 <TextInput keyboardType="decimal-pad" style={styles.input}></TextInput>
+//             </View>
+
+//             <View style={styles.btnWrapper}>
+//                 <Pressable style={styles.btn}><Text style={styles.textBtn}>Cadastrar</Text></Pressable>
+//                 <Text style={styles.loginText}>Já tem uma conta? <Text style={styles.highlightLink}>Fazer login</Text></Text>
+//             </View>
+//         </View>
+
+//     </View>
+// </View>
--- src/screens/sign-up/style.js
+++ src/screens/sign-up/style.js
@@ -1,6 +1,64 @@
+
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    gap: 90px;
+    padding: 20px;
+    `
+
+export const Container2 = styled.View`
+    gap: 48px;
+`
+
+export const Title = styled.Text`
+    font-size: 40px;
+    font-weight: bold;
+    width: 300px;
+    `
+
+export const TextHighlight = styled.Text`
+    color: #375EE7;
+    `
+
+export const FormContainer = styled.View`
+    gap: 40px;
+    `
+
+export const InputsContainer = styled.View`
+    gap: 25px;
+    `
+
+export const TextLabel = styled.Text`
+    font-size: 20px;
+    `
+
+export const Input = styled.TextInput`
+    height: 50px;
+
+    border-radius: 10px;
+    border: 1px solid #2D3133;
+    `
+
+export const Button = styled.Pressable`
+    justify-content: center;
+    align-items: center;
+    
+    height: 66px;
+    padding: 10px;
+    
+    background-color: #375EE7;
+    border-radius: 10px;
+    `
+
+export const ButtonText = styled.Text`
+    font-size: 20px;
+    font-weight: bold;
+    color: #fff;
+    `
+
 // import { StyleSheet } from "react-native";
-
-
+    
+    
 // const styles = StyleSheet.create({
 //     title: {
 //         fontSize: 40,
@@ -33,10 +91,10 @@
 //     btn: {
 //         alignItems: 'center',
 //         justifyContent: 'center',
-
+    
 //         width: '100%',
 //         height: 66,
-
+    
 //         borderRadius: 10,
 //         backgroundColor: '#375EE7'
 //     },
@@ -50,16 +108,5 @@
 //         textAlign: 'center'
 //     }
 // })
-
-// export default styles
-
-import styled from "styled-components/native";
-
-export const Container = styled.View`
-    background-color: 'red';
-`
-
-export const Title = styled.Text`
-    font-size: 40px;
     
-`
+// export default styles
